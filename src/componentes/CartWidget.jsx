8b6db0c713f55974602140506ef02753da4b9faf
Alejandro Díaz <alejandrodiazpugh@gmail.com>
2022-05-31 20:14:51 -0500

    desafio crea tu landing 2

--- src/componentes/CartWidget.jsx
+++ src/componentes/CartWidget.jsx
@@ -3,10 +3,10 @@ import { BsFillCartFill } from "react-icons/bs"
 import swal from 'sweetalert'
 
 export const CartWidget = () => {
-  
+  let counter = 0; //simulando el movimiento del counter, no es final
   return (
     <>
-    <li className='header__links' onClick={() => swal("Parece que no has agregado nada al carrito.")}><BsFillCartFill /></li>
+    <li className='header__links' onClick={() => swal("Parce, no has agregado nada al carrito.")}><BsFillCartFill /> <span className='small'>{counter}</span></li>
     </>
   )
 }
--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -3,7 +3,24 @@ import swal from 'sweetalert'
 
 export const ItemListContainer = () => {
     // Esto en teoría debería ser configurado en el container y luego mostrado en otro componente layout, no? Lo dejo así por el desafío
-    let saludos = ["Hola", "Buen día", "Buenas tardes", "Buenas noches"]
+    let saludos = ["Hola", "Buen día", "Buenas tardes", "Buenas noches"] //aqui solo estoy jugando con Date para entender como usarlo
+    let horaActual = new Date().getHours();
+    console.log(horaActual);
+    const mensajePorHora = () => {
+        console.log(horaActual)
+        switch (true) { // con el switch, evalua el caso con el parametro, entonces debe ser true, y no horaActual
+            case (horaActual <= 5):
+                return saludos[0];
+            case (horaActual <= 12):
+                return saludos[1];
+            case (horaActual <= 18):
+                return saludos[2];
+            case (horaActual <= 24):
+                return saludos[3];
+            default:
+                return saludos[0];
+        }
+    }
 
     return (
 
@@ -12,7 +29,7 @@ export const ItemListContainer = () => {
             <label htmlFor="nombre">Nombre:</label>
             <input className='m-3' type="text" name="nombre" id="nombre" />
             <br />
-            <input className='btn btn-secondary' type="button" value="Enviar" onClick={() => swal(saludos[1] + " " + document.querySelector("#nombre").value)} />
+            <input className='btn btn-secondary' type="button" value="Enviar" onClick={() => swal(mensajePorHora() + " " + document.querySelector("#nombre").value)} />
         </form>
     </div>
   )
